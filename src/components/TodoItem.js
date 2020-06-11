0556ca076b454af5ae5c6ea7d6633bc58933bb81
HMN5HU <rawath435@gmail.com>
2020-06-11 18:04:03 +0530

    made changes to TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+// fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faEdit, faTrashAlt} from'@fortawesome/free-solid-svg-icons';
+
 
 export default class TodoItem extends Component {
     render() {
+        const {title, handleDelete, handleEdit} = this.props;
         return (
-            <div>
-                hello from todo items
-            </div>
+            <li className='list-group-item text-capitalize d-flex justify-content-between my-2'>
+                <h6>{title}</h6>
+                <div className="todo-icon">
+                    <span className='mx-2 text-success' onClick={handleEdit}>
+                        <FontAwesomeIcon icon={faEdit} />
+
+                    </span>
+
+
+                    <span className='mx-2 text-danger' onClick={handleDelete}>
+                        <FontAwesomeIcon icon={faTrashAlt} />
+
+                    </span>
+                </div>
+            </li>
         )
     }
 }
