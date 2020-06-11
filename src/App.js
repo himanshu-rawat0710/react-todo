4387916fbd600815bb2923d0e62381098f191010
HMN5HU <rawath435@gmail.com>
2020-06-11 13:43:19 +0530

    added fontawesome and worked on todoInput js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
+
+
 export default class App extends Component {
   state={
     items:[{
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+// fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBook} from'@fortawesome/free-solid-svg-icons';
 
 export default class TodoInput extends Component {
     render() {
+        const {item,handleChange, handleSubmit, editItem} = this.props;
         return (
-            <h1>
-                hello from todo Input
-            </h1>
+            <div className="card card-body my-3">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <FontAwesomeIcon icon={faBook} />
+                            </div>
+                        </div>
+                        <input type='text' className='form-control text-capitalize' placeholder='add todo item' value={item} onChange={handleChange}/>
+                    </div>
+                    <button type='submit' className='btn btn-primary btn-block mt-3 text-uppercase'>
+                        Add Item
+                    </button>
+                </form>
+            </div>
         )
     }
 }
